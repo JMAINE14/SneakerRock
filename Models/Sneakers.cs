3e00b8050b51e43ca86691faab053d6a02497c59
JMAINE14 <jermainemwilkins@gmail.com>
2020-01-27 17:11:38 -0600

    First commit

---
+++ Models/Sneakers.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+namespace SneakerRock.Models
+{
+    public class Sneakers
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int ID { get; set; }
+        public int Rating { get; set; }
+
+        public int ColorWayID { get; set; }
+
+    }
+
+}
